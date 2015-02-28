ba5cfb3ee4d5ddcc69cc7c8e190aec69953b97b1
hari2095 <hari2095@gmail.com>
2015-02-28 17:28:42 +0530

    Create extractmoments.py

---
+++ extractmoments.py
@@ -0,0 +1,52 @@
+import numpy
+f=open("features.txt","r")
+##g=open("histogram_ideal.txt","r")
+#Calculate the min and max value for moments
+moms=f.read().split(" ")
+##hist=g.read().split(" ")
+minm=[]
+maxm=[]
+
+if moms[-1]=="":
+    moms.remove("")
+for i in range(7):
+    minm.append(min(numpy.float64(moms[i::7])))
+    maxm.append(max(numpy.float64(moms[i::7])))
+
+mfile=open("minm.txt","w")
+Mfile=open("maxm.txt","w")
+for m in minm:
+    mfile.write(str(m)+" ")
+
+
+
+for m in maxm:
+    Mfile.write(str(m)+" ")
+
+mfile.close()
+Mfile.close()
+#calculate the min and max for histograms
+
+##minh=[]
+##maxh=[]
+##if hist[-1]=="":
+##    hist.remove("")
+##
+##
+##for i in range(32):
+##    a=[]
+##    a=hist[i::32]
+##    a.sort()
+##    minh.append(int(a[6]))
+##    maxh.append(max(numpy.int64(hist[i::32])))
+##print minh
+##print maxh
+##mfile=open("minh.txt","w")
+##for m in minh:
+##    mfile.write(str(m)+" ")
+##mfile.close()
+##
+##Mfile=open("maxh.txt","w")
+##for m in maxh:
+##    Mfile.write(str(m)+" ")
+##Mfile.close()
